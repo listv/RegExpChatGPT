9b60c61ce74a71a3270f9b6ddf59a03d8319609a
Oleksnadr Kuziakov <akuziakov@gmail.com>
2023-06-20 10:25:31 +0300

    UC#10 Infrastructure setup UC#10 Implement regex

--- RegExpChatGpt/Program.cs
+++ RegExpChatGpt/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+    
+    
+    bool ValidateStringComplexity(string? input, int maxLength)
+    {
+        if (input == null || input.Length > maxLength)
+            return false;
 
-Console.WriteLine("Hello, World!");
+        bool hasUppercase = false;
+        bool hasLowercase = false;
+        bool hasDigit = false;
+        bool hasSpecialChar = false;
+
+        foreach (char c in input)
+        {
+            if (char.IsUpper(c))
+                hasUppercase = true;
+            else if (char.IsLower(c))
+                hasLowercase = true;
+            else if (char.IsDigit(c))
+                hasDigit = true;
+            else if ("!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~".Contains(c))
+                hasSpecialChar = true;
+
+            if (char.IsWhiteSpace(c))
+                return false;
+        }
+
+        return hasUppercase && hasLowercase && hasDigit && hasSpecialChar;
+    }
+}
