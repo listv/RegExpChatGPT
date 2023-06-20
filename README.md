c105faa26aafc2d6bbb31c74b76762843a745ceb
Oleksnadr Kuziakov <akuziakov@gmail.com>
2023-06-20 11:48:50 +0300

    UC#10 Create documentation

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 ﻿# Regular Expression for string validation
+
+## Application Description
+
+The `RegExpChatGpt` application is a C# program that focuses on string complexity validation. It provides a method called `ValidateStringComplexity` which takes an input string and a maximum length as parameters. The method checks whether the input string meets certain complexity requirements. The requirements state that the string should contain at least one uppercase letter, one lowercase letter, one digit, and one special character from the set `!"#$%&'()*+,-./:;<=>?@[]^_`{|}~`. Additionally, the string should not contain any whitespace characters.
+
+The program includes a test method called `TestValidateStringComplexity` which demonstrates the functionality of the `ValidateStringComplexity` method. It provides various test cases with different inputs and edge cases, including null input, empty input, inputs of different lengths, inputs with missing complexity elements, and inputs with whitespace characters. The test cases help ensure that the `ValidateStringComplexity` method behaves correctly and handles different scenarios appropriately.
+
+## Regex Implementation Description
+
+The ValidateStringComplexity method uses regular expressions to check for the complexity of the input string. Here's how the regex patterns are applied:
+
+1. The method first checks if the input is null or exceeds the maximum length. If either condition is true, the method returns false.
+
+2. Next, the method uses the regex pattern \s to check if the input contains any whitespace characters. If it does, the method returns false. This ensures that the input does not have any leading, trailing, or consecutive whitespace.
+
+3. The method then uses the following regex patterns to check for specific complexity requirements:
+
+   * [A-Z]: Checks if the input has at least one uppercase letter.
+   * [a-z]: Checks if the input has at least one lowercase letter.
+   * \d: Checks if the input has at least one digit (0-9).
+   * [!\"#$%&'()*+,-./:;<=>?@[\]^_{|}~]`: Checks if the input has at least one special character.
+
+4. Finally, if all the complexity requirements are met, the method returns true, indicating that the input string is considered complex.
+
+The regex patterns allow for efficient and flexible validation of the input string's complexity. By leveraging regex, the method can easily check for specific character types and patterns without the need for complex manual string manipulation.
+
+## Running the Application Locally
+
+To run the `RegExpChatGpt` application locally, follow these steps:
+
+1. Ensure you have the .NET6 SDK installed on your machine. You can download it from the official .NET website (https://dotnet.microsoft.com/download).
+
+2. Clone from [github](https://github.com/listv/RegExpChatGPT.git) an existing project using your preferred Integrated Development Environment (IDE) that supports C# development (e.g., Visual Studio, Visual Studio Code with C# extensions).
+
+4. Build the project to compile the code and resolve any dependencies.
+
+5. Once the project is built successfully, you can run the `Main` method to execute the `TestValidateStringComplexity` method and see the test results in the console.
+
+Note: Make sure to review and adjust the test cases or modify the code as needed to fit your specific requirements.
--- RegExpChatGpt/Program.cs
+++ RegExpChatGpt/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Text.RegularExpressions;
+
 namespace RegExpChatGpt;
 
 public static class Program
@@ -8,71 +9,58 @@ public static class Program
         TestValidateStringComplexity();
     }
 
-    static void TestValidateStringComplexity()
+    private static void TestValidateStringComplexity()
     {
         // Test cases with different inputs and edge cases
-        string[] testInputs = {
+        string?[] testInputs =
+        {
             // Existing test cases
-            null,                 // Null input
-            "",                   // Empty input
-            "abc",                // Too short input (less than maxLength)
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",  // Missing lowercase
-            "abcdefghijklmnopqrstuvwxyz",  // Missing uppercase
-            "1234567890",         // Missing special character
-            "!@#$%^&*()_+",       // Missing digit
-            "Abcdef!1",           // Matching input length
-            " Abcdef!1",          // Leading whitespace
-            "Abcdef!1 ",          // Trailing whitespace
-            " Abcdef!1 ",         // Leading and trailing whitespace
-            "A bcdef!1",          // Whitespace in between
-            "Abcdef !1",          // Whitespace before special character
-            "Abcdef! 1",          // Whitespace after special character
-            "A b c d e f! 1",     // Whitespace in various positions
+            null, // Null input
+            "", // Empty input
+            "abc", // Too short input (less than maxLength)
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ", // Missing lowercase
+            "abcdefghijklmnopqrstuvwxyz", // Missing uppercase
+            "1234567890", // Missing special character
+            "!@#$%^&*()_+", // Missing digit
+            "Abcdef!1", // Matching input length
+            " Abcdef!1", // Leading whitespace
+            "Abcdef!1 ", // Trailing whitespace
+            " Abcdef!1 ", // Leading and trailing whitespace
+            "A bcdef!1", // Whitespace in between
+            "Abcdef !1", // Whitespace before special character
+            "Abcdef! 1", // Whitespace after special character
+            "A b c d e f! 1", // Whitespace in various positions
             "A  b  c  d  e  f!1", // Multiple consecutive whitespaces
 
             // Additional test cases for tabulation symbols, newline symbols, and line break symbols
-            "Abc\tdef!1",         // Tabulation symbol
-            "Abc\ndef!1",         // Newline symbol
-            "Abc\rdef!1",         // Carriage return symbol
-            "Abc\n\rdef!1",       // Newline and carriage return symbols
+            "Abc\tdef!1", // Tabulation symbol
+            "Abc\ndef!1", // Newline symbol
+            "Abc\rdef!1", // Carriage return symbol
+            "Abc\n\rdef!1", // Newline and carriage return symbols
             "A\nb\rc\td\ne\r\nf!1" // Mix of whitespace symbols
         };
 
-        int maxLength = 10;
+        var maxLength = 10;
 
-        foreach (string input in testInputs)
+        foreach (var input in testInputs)
         {
-            bool isValid = ValidateStringComplexity(input, maxLength);
+            var isValid = ValidateStringComplexity(input, maxLength);
             Console.WriteLine($"Input: \"{input}\" | Valid: {isValid}");
         }
     }
 
-
-
-    static bool ValidateStringComplexity(string? input, int maxLength)
+    private static bool ValidateStringComplexity(string? input, int maxLength)
     {
         if (input == null || input.Length > maxLength)
             return false;
 
-        bool hasUppercase = false;
-        bool hasLowercase = false;
-        bool hasDigit = false;
-        bool hasSpecialChar = false;
+        if (Regex.IsMatch(input, @"\s"))
+            return false;
 
-        foreach (char c in input)
-        {
-            if (char.IsUpper(c))
-                hasUppercase = true;
-            else if (char.IsLower(c))
-                hasLowercase = true;
-            else if (char.IsDigit(c))
-                hasDigit = true;
-            else if ("!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~".Contains(c))
-                hasSpecialChar = true;
-
-            if (char.IsWhiteSpace(c))
-                return false;
-        }
+        var hasUppercase = Regex.IsMatch(input, @"[A-Z]");
+        var hasLowercase = Regex.IsMatch(input, @"[a-z]");
+        var hasDigit = Regex.IsMatch(input, @"\d");
+        var hasSpecialChar = Regex.IsMatch(input, @"[!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~]");
 
         return hasUppercase && hasLowercase && hasDigit && hasSpecialChar;
     }
