9ed08bffa6760a92415aa3b012b13a1c46826277
Oleksnadr Kuziakov <akuziakov@gmail.com>
2023-06-20 11:03:00 +0300

    UC#10 Implement regex tests

--- RegExpChatGpt/Program.cs
+++ RegExpChatGpt/Program.cs
@@ -3,10 +3,50 @@ namespace RegExpChatGpt;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        
+        TestValidateStringComplexity();
     }
+
+    static void TestValidateStringComplexity()
+    {
+        // Test cases with different inputs and edge cases
+        string[] testInputs = {
+            // Existing test cases
+            null,                 // Null input
+            "",                   // Empty input
+            "abc",                // Too short input (less than maxLength)
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",  // Missing lowercase
+            "abcdefghijklmnopqrstuvwxyz",  // Missing uppercase
+            "1234567890",         // Missing special character
+            "!@#$%^&*()_+",       // Missing digit
+            "Abcdef!1",           // Matching input length
+            " Abcdef!1",          // Leading whitespace
+            "Abcdef!1 ",          // Trailing whitespace
+            " Abcdef!1 ",         // Leading and trailing whitespace
+            "A bcdef!1",          // Whitespace in between
+            "Abcdef !1",          // Whitespace before special character
+            "Abcdef! 1",          // Whitespace after special character
+            "A b c d e f! 1",     // Whitespace in various positions
+            "A  b  c  d  e  f!1", // Multiple consecutive whitespaces
+
+            // Additional test cases for tabulation symbols, newline symbols, and line break symbols
+            "Abc\tdef!1",         // Tabulation symbol
+            "Abc\ndef!1",         // Newline symbol
+            "Abc\rdef!1",         // Carriage return symbol
+            "Abc\n\rdef!1",       // Newline and carriage return symbols
+            "A\nb\rc\td\ne\r\nf!1" // Mix of whitespace symbols
+        };
+
+        int maxLength = 10;
+
+        foreach (string input in testInputs)
+        {
+            bool isValid = ValidateStringComplexity(input, maxLength);
+            Console.WriteLine($"Input: \"{input}\" | Valid: {isValid}");
+        }
+    }
+
 
 
     static bool ValidateStringComplexity(string? input, int maxLength)
